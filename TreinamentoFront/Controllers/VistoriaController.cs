a52fb3fcede442e7c81ae7bbc44d703be5d4a512
caicrs <caicrs.contact@gmail.com>
2023-05-22 11:55:24 -0300

    New template - Page manage vistoria created

---
+++ TreinamentoFront/Controllers/VistoriaController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using TreinamentoFront.Models;
+
+namespace TreinamentoFront.Controllers
+{
+    public class VistoriaController : Controller
+    {
+        private readonly ILogger<VistoriaController> _logger;
+
+        public VistoriaController(ILogger<VistoriaController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Create()
+        {
+            ViewData["Message"] = "Your application create page.";
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ TreinamentoFront/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TreinamentoFront.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
