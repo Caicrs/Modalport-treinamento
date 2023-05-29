4ba2d6d1ef8955910e2332cc193577c50f1b88a1
caicrs <caicrs.contact@gmail.com>
2023-05-29 11:00:10 -0300

    Models - Dropdown list models created

---
+++ TreinamentoFront/Models/ItensOptions.cs
@@ -0,0 +1,27 @@
+﻿namespace TreinamentoFront.Models
+{
+    public class ItensOptions
+    {
+        // Define your GetCities method here
+        public static List<string> GetItemTypeList()
+        {
+            // Perform logic to retrieve the list of cities
+            // For demonstration purposes, let's return a sample list of cities
+            List<string> typesList = new List<string>
+        {
+            "1 - Foto",
+            "2 - Texto",
+            "3 - Multipla escolha",
+            "4 - Numérico",
+            "5 - Assinatura",
+            "6 - Única Escolha"
+        };
+
+            return typesList;
+        }
+
+        public string CustomClass { get; set; }
+        public string DropDownCssClass { get; set; }
+        public List<string> HobbyList { get; set; }
+    }
+}
