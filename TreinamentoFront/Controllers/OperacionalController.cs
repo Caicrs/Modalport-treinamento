d381d0a24e4742f5f2ddbd226925710567b7993a
caicrs <caicrs.contact@gmail.com>
2023-06-06 15:49:50 -0300

    Pages - Cadastros modal input function inserted

--- TreinamentoFront/Controllers/OperacionalController.cs
+++ TreinamentoFront/Controllers/OperacionalController.cs
@@ -27,6 +27,10 @@ namespace TreinamentoFront.Controllers
         {
             return View();
         }
+        public IActionResult Processos()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
